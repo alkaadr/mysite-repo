eb8516262f3c28f36f7deb2784b7cb3c27f93c8d
a_kazliak <aliaksei.kazliak@gmail.com>
2021-11-15 19:35:48 +0300

    Changed the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -12,11 +12,14 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+[...]
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
